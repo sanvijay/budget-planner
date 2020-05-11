a18da54fda767dad9a81507866cd81b10b007757
svijaya7 <vijayaksa@advisory.com>
2020-05-11 11:43:03 +0530

    Update Quiz.vue

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -16,7 +16,7 @@
       <div class="row">
         <div class="col-md-3 col-lg-3"></div>
         <div class="col-sm-12 col-md-6 col-lg-6 bg-light shadow-lg">
-          Get your <router-link :to="{ name: 'register' }">finsey account</router-link> now for free.<br>
+          Get your <router-link :to="{ name: 'register' }">account</router-link> now for free.<br>
           <br>
         </div>
         <div class="col-md-3 col-lg-3"></div>
